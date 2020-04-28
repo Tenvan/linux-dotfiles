8ac232e36c4073ac5d45993025fd386604ff20d8
stira <ralf.stich@infoniqa.com>
2020-04-28 15:16:08 +0200

    final definition

--- i3install.sh
+++ i3install.sh
@@ -1,3 +1,6 @@
-yay -S morc_menu bmenu rofi zenity compton dunst i3-scrot spectacle polkit-gnome polkit-kde-agent xfce4-power-manager pamac-tray-appindicator clipit xautolock
+yay -S --noconfirm morc_menu bmenu rofi zenity compton dunst i3-scrot spectacle polkit-gnome polkit-kde-agent xfce4-power-manager pamac-tray-appindicator clipit xautolock conky conky-i3 bumblebee-status pa-applet volumeicon
+
+yay -S --noconfirm checkupdates-aur python-i3ipc python-psutil xcwd python-pygit2 python-xkbgroup xkb-switch-i3 progress python-taskw python-requests 
+
 
 # mocp (???)
