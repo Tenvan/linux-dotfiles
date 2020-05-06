7d747a738fcddc96f58c716b261896031552bf28
stira <ralf.stich@infoniqa.com>
2020-05-06 10:27:15 +0200

    make light/dark more switchable

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # i3-desktop
 
-Komplette Konfiguration eines I3-Desktops.
+- Komplette Konfiguration eines I3-Desktops.
+- Fest ausgelegt auf Dual-Monitor
+- Umschaltung zwischen Light/Dark Theme durch Git-Branch
+
+Als Basis dient eine Full-I3 Installation durch den Manjaro "Architekt"-Installater, oder eine vergleichbare.
+
+## Installation
+
+Fehlende Pakete werden mit folgendem Batch nachinstalliert:
+
+```
+sh ~/i3install.sh
+```
+
+## Manuelle Konfiguration
+
+Einzige z.Zt. nötige manuelle Konfiguration ist die Anschlussdefinitiob der beiden Monitore.
+
+Hierfür muss im Homeverzeichnis eine Datei '.Xresources.monitor' angelegt werden, die folgenden Aufbau hat:
+
+```
+*monitor1: DP2
+*monitor2: DP3
+```
+
+Ermittelt werden können diese durch:
+
+```
+xrandr | grep connected -w 
+```
+
+Rebooten bzw- neu anmdelden nicht vergessen !
--- i3install.sh
+++ i3install.sh
@@ -1,11 +1,15 @@
 # base i3 with bumblebee-status
-yay -S bmenu perl-anyevent-i3 rofi zenity compton dunst i3exit spectacle polkit-gnome polkit-kde-agent \
-	pamac-tray-appindicator clipit xautolock conky conky-i3 bumblebee-status pavucontrol pa-applet \
-	dmenu-manjaro checkupdates-aur python-i3ipc python-psutil xcwd-git python-pygit2 python-xkbgroup \
-	xkb-switch-i3 progress python-taskw python-requests octopi octopi-notifier-qt5 xautolock xorg-xfontsel \
-	kate krusader spectacle
-	
-# For polybar
-yay -S polybar jq ttf-polybar-icons ttf-weather-icons polybar-scripts-git 
+yay -S bmenu perl-anyevent-i3 rofi zenity compton dunst i3exit spectacle polkit-gnome polkit-kde-agent pamac-tray-appindicator clipit xautolock conky conky-i3 bumblebee-status pavucontrol pa-applet dmenu-manjaro checkupdates-aur python-i3ipc python-psutil xcwd-git python-pygit2 python-xkbgroup xkb-switch-i3 progress python-taskw python-requests octopi octopi-notifier-qt5 xautolock xorg-xfontsel kate krusader spectacle 
+
+# arts
+yay -S manjaro-artwork manjaro-artwork-extra artwork-i3 i3-default-artwork illyria-wallpaper manjaro-wallpapers-17.0 manjaro-wallpapers-18.0 wallpapers-juhraya cinnamon-wallpapers manjaro-wallpapers-by-lunix-cinnamon manjaro-wallpapers-by-lunix-i3 yaru-colors-wallpapers-git manjaro-users-artwork-wallpapers muser-wallpapers
+
+# cursor
+yay -S xcursor-chameleon-pearl xcursor-chameleon-darkskyblue xcursor-chameleon-skyblue xcursor-chameleon-anthracite xcursor-chameleon-white
+
+# icons
+yay -S manjaro-artwork-icons ttf-weather-icons 
 
 
+chmod +x ~/100-user-monitors.sh
+sudo cp ~/100-user-monitors.sh /etc/X11/xinit/xinitrc.d
