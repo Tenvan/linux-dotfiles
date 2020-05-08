84190a7aae47b6c77930342ee68a5492e2da7691
stira <ralf.stich@infoniqa.com>
2020-05-08 09:41:19 +0200

    add ungit autostart

--- i3install.sh
+++ i3install.sh
@@ -1,5 +1,8 @@
 # base i3 with bumblebee-status
-yay -S --noconfirm bmenu perl-anyevent-i3 rofi zenity compton dunst i3exit spectacle polkit-gnome polkit-kde-agent pamac-tray-appindicator clipit xautolock conky conky-i3 bumblebee-status pavucontrol pa-applet dmenu-manjaro checkupdates-aur python-i3ipc python-psutil xcwd-git python-pygit2 python-xkbgroup xkb-switch-i3 progress python-taskw python-requests octopi octopi-notifier-qt5 xautolock xorg-xfontsel kate krusader spectacle kde-cli-tools ksystemlog ksysguard gnome-system-monitor gnome-system-log xclip xsel xdotool bitwarden-bin bitwarden-cli-bin bitwarden-rofi
+yay -S --noconfirm bmenu perl-anyevent-i3 rofi zenity compton dunst i3exit spectacle polkit-gnome polkit-kde-agent pamac-gtk pamac-cli pamac-tray-appindicator clipit xautolock conky conky-i3 bumblebee-status pavucontrol pa-applet dmenu-manjaro checkupdates-aur python-i3ipc python-psutil xcwd-git python-pygit2 python-xkbgroup xkb-switch-i3 progress python-taskw python-requests octopi octopi-notifier-qt5 kate krusader spectacle kde-cli-tools ksystemlog ksysguard gnome-system-monitor gnome-system-log xclip xsel xdotool bitwarden-bin bitwarden-cli-bin bitwarden-rofi
+
+# applications
+yay -S ungit
 
 # arts
 yay -S --noconfirm manjaro-artwork manjaro-artwork-extra artwork-i3 i3-default-artwork illyria-wallpaper manjaro-wallpapers-17.0 manjaro-wallpapers-18.0 wallpapers-juhraya cinnamon-wallpapers manjaro-wallpapers-by-lunix-cinnamon manjaro-wallpapers-by-lunix-i3 yaru-colors-wallpapers-git manjaro-users-artwork-wallpapers muser-wallpapers
