ced493ebebaea6f92c3a57746f61a3b1f011a802
stira <ralf.stich@infoniqa.com>
2020-05-08 12:42:43 +0200

    some tuning

--- i3install.sh
+++ i3install.sh
@@ -1,8 +1,11 @@
 # base i3 with bumblebee-status
-yay -S --noconfirm bmenu perl-anyevent-i3 rofi zenity compton dunst i3exit spectacle polkit-gnome polkit-kde-agent pamac-gtk pamac-cli pamac-tray-appindicator clipit xautolock conky conky-i3 bumblebee-status pavucontrol pa-applet dmenu-manjaro checkupdates-aur python-i3ipc python-psutil xcwd-git python-pygit2 python-xkbgroup xkb-switch-i3 progress python-taskw python-requests octopi octopi-notifier-qt5 kate krusader spectacle kde-cli-tools ksystemlog ksysguard gnome-system-monitor gnome-system-log xclip xsel xdotool bitwarden-bin bitwarden-cli-bin bitwarden-rofi
+yay -S --noconfirm bmenu perl-anyevent-i3 rofi zenity compton dunst i3exit spectacle gksudo polkit-gnome  pamac-gtk pamac-cli pamac-tray-appindicator clipit xautolock conky conky-i3 bumblebee-status pavucontrol pa-applet dmenu-manjaro checkupdates-aur python-i3ipc python-psutil xcwd-git python-pygit2 python-xkbgroup xkb-switch-i3 progress python-taskw python-requests octopi octopi-notifier-qt5 xfce4-taskmanager gnome-system-monitor gnome-system-log xclip xsel xdotool bitwarden-bin bitwarden-cli-bin bitwarden-rofi lightdm-gtk-greeter-settings
+
+# from kde (replace if possible)
+yay -S --noconfirm polkit-kde-agent kate krusader kde-cli-tools ksystemlog ksysguard
 
 # applications
-yay -S ungit gitahead-bin
+yay -S --noconfirm ungit gitahead-bin timeset-gui
 
 # arts
 yay -S --noconfirm manjaro-artwork manjaro-artwork-extra artwork-i3 i3-default-artwork illyria-wallpaper manjaro-wallpapers-17.0 manjaro-wallpapers-18.0 wallpapers-juhraya cinnamon-wallpapers manjaro-wallpapers-by-lunix-cinnamon manjaro-wallpapers-by-lunix-i3 yaru-colors-wallpapers-git manjaro-users-artwork-wallpapers muser-wallpapers
