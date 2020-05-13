3e9d85c54366c6071792b41e20693ffbeec44377
stira <ralf.stich@infoniqa.com>
2020-05-13 17:36:56 +0200

    add awesome-wm-widgets

--- .config/awesome/bars.lua
+++ .config/awesome/bars.lua
@@ -7,6 +7,10 @@
 -- Standard Definitions
 require("definitions")
 
+-- awesome-wm-widgets
+local battery_widget   = require("awesome-wm-widgets.battery-widget.battery")
+local cpu_widget       = require("awesome-wm-widgets.cpu-widget.cpu-widget")
+
 -- Standard awesome library
 local gears            = require("gears")
 local awful            = require("awful")
@@ -26,40 +30,40 @@ mytextclock            = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
 local taglist_buttons  = gears.table.join(
-        awful.button({}, 1, function(t)
-          t:view_only()
-        end),
-        awful.button({ modkey }, 1, function(t)
-          if client.focus then
-            client.focus:move_to_tag(t)
-          end
-        end), awful.button({}, 3, awful.tag.viewtoggle),
-        awful.button({ modkey }, 3, function(t)
-          if client.focus then
-            client.focus:toggle_tag(t)
-          end
-        end), awful.button({}, 4, function(t)
-          awful.tag.viewnext(t.screen)
-        end),
-        awful.button({}, 5, function(t)
-          awful.tag.viewprev(t.screen)
-        end))
+  awful.button({}, 1, function(t)
+    t:view_only()
+  end),
+  awful.button({ modkey }, 1, function(t)
+    if client.focus then
+      client.focus:move_to_tag(t)
+    end
+  end), awful.button({}, 3, awful.tag.viewtoggle),
+  awful.button({ modkey }, 3, function(t)
+    if client.focus then
+      client.focus:toggle_tag(t)
+    end
+  end), awful.button({}, 4, function(t)
+    awful.tag.viewnext(t.screen)
+  end),
+  awful.button({}, 5, function(t)
+    awful.tag.viewprev(t.screen)
+  end))
 
 local tasklist_buttons = gears.table.join(
-        awful.button({}, 1, function(c)
-          if c == client.focus then
-            c.minimized = true
-          else
-            c:emit_signal("request::activate", "tasklist", { raise = true })
-          end
-        end), awful.button({}, 3, function()
-          awful.menu.client_list({ theme = { width = 250 } })
-        end), awful.button({}, 4, function()
-          awful.client.focus.byidx(1)
-        end),
-        awful.button({}, 5, function()
-          awful.client.focus.byidx(-1)
-        end))
+  awful.button({}, 1, function(c)
+    if c == client.focus then
+      c.minimized = true
+    else
+      c:emit_signal("request::activate", "tasklist", { raise = true })
+    end
+  end), awful.button({}, 3, function()
+    awful.menu.client_list({ theme = { width = 250 } })
+  end), awful.button({}, 4, function()
+    awful.client.focus.byidx(1)
+  end),
+  awful.button({}, 5, function()
+    awful.client.focus.byidx(-1)
+  end))
 
 local function set_wallpaper(s)
   -- Wallpaper
@@ -102,20 +106,20 @@ awful.screen.connect_for_each_screen(function(s)
   -- We need one layoutbox per screen.
   s.mylayoutbox = awful.widget.layoutbox(s)
   s.mylayoutbox:buttons(gears.table.join(
-          awful.button({}, 1,
-                       function()
-                         awful.layout.inc(1)
-                       end),
-          awful.button({}, 3,
-                       function()
-                         awful.layout.inc(-1)
-                       end), awful.button({}, 4, function()
-            awful.layout.inc(1)
-          end),
-          awful.button({}, 5,
-                       function()
-                         awful.layout.inc(-1)
-                       end)))
+    awful.button({}, 1,
+                 function()
+                   awful.layout.inc(1)
+                 end),
+    awful.button({}, 3,
+                 function()
+                   awful.layout.inc(-1)
+                 end), awful.button({}, 4, function()
+      awful.layout.inc(1)
+    end),
+    awful.button({}, 5,
+                 function()
+                   awful.layout.inc(-1)
+                 end)))
   -- Create a taglist widget
   s.mytaglist  = awful.widget.taglist {
     screen  = s,
@@ -145,9 +149,16 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytasklist, -- Middle widget
     { -- Right widgets
       layout = wibox.layout.fixed.horizontal,
+      cpu_widget({
+                   width        = 120,
+                   step_width   = 2,
+                   step_spacing = 0,
+                   color        = '#434c5e'
+                 }),
       myKeyboardLayout,
       wibox.widget.systray(),
       mytextclock,
+      --battery_widget(),
       s.mylayoutbox
     }
   }
