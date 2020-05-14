6be8da8e71685503ba9d73d0f2f13a47a1468586
stira <ralf.stich@infoniqa.com>
2020-05-14 10:54:34 +0200

    fix dark color (finish)

--- .config/fish/custom.fish
+++ .config/fish/custom.fish
@@ -3,8 +3,6 @@
 
 # -------------------------------------------------
 #
-# BobTheFish:
-#
 
 set -g theme_nerd_fonts yes
 set -g theme_powerline_fonts yes
@@ -34,6 +32,34 @@ set -g default_user your_normal_user
 set -g theme_color_scheme zenburn
 # set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 0
+
+#
+# Theme
+#
+set -g fish_color_normal normal
+
+set -g fish_color_command 99cc99
+set -g fish_color_quote ffcc66
+set -g fish_color_redirection d3d0c8
+set -g fish_color_end cc99cc
+set -g fish_color_error f2777a
+set -g fish_color_param d3d0c8
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_history_current --bold
+set -g fish_color_operator 6699cc
+set -g fish_color_escape 66cccc
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_valid_path --underline
+set -g fish_color_autosuggestion 747369
+set -g fish_color_user brgreen
+set -g fish_color_host normal
+set -g fish_color_cancel -r
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description B3A06D yellow
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
 
 #
 # -------------------------------------------------
