9ee1bf3ed3bf2f2e680b2974b3a47de03ab807db
stira <ralf.stich@infoniqa.com>
2020-05-15 19:04:04 +0200

    fix theme scripts
    (cherry picked from commit f497f23cbd65a5e59e8c61ec4524920dd7f66f47)

--- Scripts/darktheme.sh
+++ Scripts/darktheme.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cd ~
-git checkout dark-theme -f;
-xrdb -merge ~/.Xresources;
-notif-send -u critical 'Auf Dark-Theme gewechselt. QT5 Anwendungen neu starten.'
+git checkout dark-theme -f
+xrdb -merge ~/.Xresources
--- Scripts/lighttheme.sh
+++ Scripts/lighttheme.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cd ~
-git checkout light-theme -f;
-xrdb -merge ~/.Xresources;
-notif-send -u critical 'Auf Light-Theme gewechselt. QT5 Anwendungen neu starten.'
+git checkout light-theme -f
+xrdb -merge ~/.Xresources
