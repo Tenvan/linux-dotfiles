3fb3d2b931a10fa9ae3e52b151c5679f7ca1298e
stira <ralf.stich@infoniqa.com>
2020-05-29 12:07:50 +0200

    try to fix picom flicker (all)

--- Scripts/install_base.sh
+++ Scripts/install_base.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# base i3 with bumblebee-status
-yay -S picom-ibhagwan-git
+
+#yay -S picom
+#yay -S picom-ibhagwan-git
+yay -S picom-tryone-git
 
 yay -S termite termite-style-git nitrogen dmenu bmenu rofi zenity ripgrep \
 exa glances bashtop gtop htop iftop iotop iptraf-ng s-tui \
@@ -20,7 +22,7 @@ yay -S kate geany geany-plugins geany-themes editorconfig-geany neovim vundle
 yay -S xfce4 manjaro-xfce-settings xfce4-panel
 
 yay -S xfce4-taskmanager xfce4-panel xfce4-panel-profiles xfce4-whiskermenu-plugin \
-xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-diskperf-plugin xfce4-mount-plugin \ 
+xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-diskperf-plugin xfce4-mount-plugin \
 xfce4-sensors-plugin xfce4-taskmanager xfce4-systemload-plugin xfce4-weather-plugin \
 xfce4-timer-plugin xfce4-indicator-plugin-git xfce4-calculator-plugin
 
--- Scripts/picom-toggle.sh
+++ Scripts/picom-toggle.sh
@@ -3,5 +3,9 @@ if pgrep -x "picom" > /dev/null
 then
 	killall picom
 else
-	picom -b --config ~/.config/picom/picom.conf
+    if [ -f $HOME/.config/picom/picom-private.conf ] ; then
+        picom --config $HOME/.config/picom/picom-private.conf &
+    elif [ -f $HOME/.config/picom/picom.conf ] ; then
+            picom --config $HOME/.config/picom/picom.conf &
+    fi
 fi
