0976e706e20ee60076da3175fab328555046c705
stira <ralf.stich@infoniqa.com>
2020-09-23 19:27:56 +0200

    change some settings (all)

--- Scripts/autostart-global.sh
+++ Scripts/autostart-global.sh
@@ -29,8 +29,8 @@ xsetroot -cursor_name left_ptr &
 ######################
 
 # nitrogen --restore
-nitrogen --set-zoom-fill ~/.local/share/wallpapers/shared/blue-earth-2880x1800.jpg --head=0
-nitrogen --set-zoom-fill ~/.local/share/wallpapers/shared/floating-world-in-space-3000x2357.jpg --head=1
+nitrogen --set-zoom-fill ~/.local/share/wallpapers/shared/blue-earth-2880x1800.jpg --head=1
+nitrogen --set-zoom-fill ~/.local/share/wallpapers/shared/floating-world-in-space-3000x2357.jpg --head=0
 # run variety &
 
 # Some ways to set your wallpaper besides variety or nitrogen
