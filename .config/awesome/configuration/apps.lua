172340ea1d35d7d309e00c9be94929f16c403ca1
stira <ralf.stich@infoniqa.com>
2022-06-21 22:14:53 +0200

    new awesome theme ryhyn

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -1,76 +1,39 @@
-local filesystem = require('gears.filesystem')
+local filesystem = require("gears.filesystem")
 local config_dir = filesystem.get_configuration_dir()
-local utils_dir = config_dir .. 'utilities/'
+local utils_dir = config_dir .. "utilities/"
 
 return {
-	-- The default applications that we will use in keybindings and widgets
+	-- Default Applications
 	default = {
 		-- Default terminal emulator
-		terminal = 'kitty',
-		-- Default web browser
-		web_browser = 'firefox',
+		terminal = "alacritty",
+		-- Defalut music client
+		music_player = "alacritty --class music -e ncmpcpp",
 		-- Default text editor
-		text_editor = 'subl3',
+		text_editor = "alacritty -e nvim",
+		-- Default code editor
+		code_editor = "code",
+		-- Default web browser
+		web_browser = "firefox",
 		-- Default file manager
-		file_manager = 'dolphin',
-		-- Default media player
-		multimedia = 'vlc',
-		-- Default game, can be a launcher like steam
-		game = 'supertuxkart',
-		-- Default graphics editor
-		graphics = 'gimp-2.10',
-		-- Default sandbox
-		sandbox = 'virtualbox',
-		-- Default IDE
-		development = '',
+		file_manager = "nautilus",
 		-- Default network manager
-		network_manager = 'kitty iwctl',
+		network_manager = "alacritty -e nmtui",
 		-- Default bluetooth manager
-		bluetooth_manager = 'blueman-manager',
+		bluetooth_manager = "blueman-manager",
 		-- Default power manager
-		power_manager = 'xfce4-power-manager',
-		-- Default GUI package manager
-		package_manager = 'pamac-manager',
-		-- Default locker
-		lock = 'awesome-client "awesome.emit_signal(\'module::lockscreen_show\')"',
-		-- Default quake terminal
-		quake = 'kitty --name QuakeTerminal',
-
-    -- You can add more default applications here
-	},
-
-	-- List of apps to start once on start-up
-	run_on_start_up = {
-		-- Compositor
-		'picom -b --experimental-backends --dbus --config ' ..
-		config_dir .. '/configuration/picom.conf',
-		-- Blueman applet
-		'blueman-applet',
-		-- Music server
-		'mpd',
-		-- Polkit and keyring
-		'/usr/bin/lxqt-policykit-agent &' ..
-		' eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)',
-		-- Load X colors
-		'xrdb $HOME/.Xresources',
-		-- Audio equalizer
-		'pulseeffects --gapplication-service',
-		-- Lockscreen timer
-		[[
-		xidlehook --not-when-fullscreen --not-when-audio --timer 600 \
-		"awesome-client 'awesome.emit_signal(\"module::lockscreen_show\")'" ""
-		]]
-
-		-- You can add more start-up applications here
+		power_manager = "xfce4-power-manager",
+		-- Default rofi global menu
+		app_launcher = "rofi -no-lazy-grab -show drun -modi drun -theme " .. config_dir .. "configuration/rofi.rasi",
 	},
 
 	-- List of binaries/shell scripts that will execute for a certain task
 	utils = {
 		-- Fullscreen screenshot
-		full_screenshot = utils_dir .. 'snap full',
+		full_screenshot = utils_dir .. "screensht full",
 		-- Area screenshot
-		area_screenshot = utils_dir .. 'snap area',
-		-- Update profile picture
-		update_profile  = utils_dir .. 'profile-image'
-	}
+		area_screenshot = utils_dir .. "screensht area",
+		-- Color Picker
+		color_picker = utils_dir .. "xcolor-pick",
+	},
 }
