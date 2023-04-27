cabe52adb429c52e43d94d5d33f0f29ed9ecc88a
stira <ralf.stich@infoniqa.com>
2023-04-27 17:31:01 +0200

    add widget config (finish)

--- .config/awesome/configuration/config-system.lua
+++ .config/awesome/configuration/config-system.lua
@@ -53,10 +53,6 @@ local config = {
       -- FPS
       fps = '30'
     },
-    spotify = {
-      enabled = false,
-      screen = 1
-    },
     harddrives = {
       mounts = {
         '/',
@@ -66,48 +62,96 @@ local config = {
     seperator = {
       enabled = true
     },
-    layoutbox = {
-      enabled = true,
-      screen = 1
-    }
-  },
-  module = {
-    auto_start = {
-      -- Will create notification if true
-      debug_mode = true
-    },
-    dynamic_wallpaper = {
-      -- Will look for wallpapers here
-      wall_dir = 'theme/wallpapers/',
-      -- Image formats
-      valid_picture_formats = { 'jpg', 'png', 'jpeg' },
-      -- Leave this table empty for full auto scheduling
-      wallpaper_schedule = {
-        -- ['00:00:00'] = 'midnight-wallpaper.jpg',
-        -- ['06:22:00'] = 'morning-wallpaper.jpg',
-        -- ['12:00:00'] = 'noon-wallpaper.jpg',
-        -- ['17:58:00'] = 'night-wallpaper.jpg',
+    left_widgets = {
+      -- all screens
+      {
+        'layout_box',
+        'fire_icon',
+        'cpu',
+        'ram',
+      },
+      -- screen 1
+      {
+      },
+      -- screen 2
+      {
+        'color_palette',
+        'net_icon',
+        'net_speed',
+        'media_icon',
+        'spotify',
       }
     },
-    lockscreen = {
-      -- Clock format
-      military_clock = true,
-      -- Default password if there's no PAM integration
-      fallback_password = 'time',
-      -- Capture intruder using webcam
-      capture_intruder = true,
-      -- Intruder image save location (Will create directory if it doesn't exist)
-      face_capture_dir = '$(xdg-user-dir PICTURES)/Intruders/',
-      -- Background directory - Defaults to 'awesome/config/theme/wallpapers/' if null
-      bg_dir = nil,
-      -- Will look for this image file under 'bg_dir'
-      bg_image = 'locksreen-bg.jpg',
-      -- Blur lockscreen background
-      blur_background = false,
-      -- Blurred/filtered background image path (No reason to change this)
-      tmp_wall_dir = '/tmp/awesomewm/' .. os.getenv('USER') .. '/'
-    }
-  },
+    center_widgets = {
+      -- all screens
+      {
+      },
+      -- screen 1
+      {
+        'weather',
+      },
+      -- screen 2
+      {
+      }
+    },
+    right_widgets = {
+      -- all screens
+      {
+        'screen_rec',
+      },
+      -- screen 1
+      {
+        'updater',
+        'hard_drives',
+        'systray',
+        'clock_icon',
+        'clock',
+        'info_center_toggle',
+      },
+      -- screen 2
+      {
+        'clock_icon',
+        'clock',
+      }
+    },
+    module = {
+      auto_start = {
+        -- Will create notification if true
+        debug_mode = true
+      },
+      dynamic_wallpaper = {
+        -- Will look for wallpapers here
+        wall_dir = 'theme/wallpapers/',
+        -- Image formats
+        valid_picture_formats = { 'jpg', 'png', 'jpeg' },
+        -- Leave this table empty for full auto scheduling
+        wallpaper_schedule = {
+          -- ['00:00:00'] = 'midnight-wallpaper.jpg',
+          -- ['06:22:00'] = 'morning-wallpaper.jpg',
+          -- ['12:00:00'] = 'noon-wallpaper.jpg',
+          -- ['17:58:00'] = 'night-wallpaper.jpg',
+        }
+      },
+      lockscreen = {
+        -- Clock format
+        military_clock = true,
+        -- Default password if there's no PAM integration
+        fallback_password = 'time',
+        -- Capture intruder using webcam
+        capture_intruder = true,
+        -- Intruder image save location (Will create directory if it doesn't exist)
+        face_capture_dir = '$(xdg-user-dir PICTURES)/Intruders/',
+        -- Background directory - Defaults to 'awesome/config/theme/wallpapers/' if null
+        bg_dir = nil,
+        -- Will look for this image file under 'bg_dir'
+        bg_image = 'locksreen-bg.jpg',
+        -- Blur lockscreen background
+        blur_background = false,
+        -- Blurred/filtered background image path (No reason to change this)
+        tmp_wall_dir = '/tmp/awesomewm/' .. os.getenv('USER') .. '/'
+      }
+    },
+  }
 }
 
 return config
--- .config/awesome/layout/top-panel.lua
+++ .config/awesome/layout/top-panel.lua
@@ -33,106 +33,117 @@ local top_panel = function(s, offset)
     end
   end)
 
-  local clock              = require('widget.clock')(s)
-  local layout_box         = require('widget.layoutbox')(s)
-  local hard_drives        = require('widget.harddrive-meter').widget
-  local weather            = require('widget.weather.weather-aw').widget
-  local systray            = require('widget.systray').widget
-  local spotify_widget     = require('widget.spotify')
-  local updater            = require('widget.package-updater')()
-  local screen_rec         = require('widget.screen-recorder')()
-  local info_center_toggle = require('widget.info-center-toggle')()
-  local net_speed_widget   = require('widget.network-speed')
-  local toolbar_ram_widget = require('widget.ram-widget.ram-widget')({
-    -- width = dpi(200)
-  })
-
-  local seperator          = {
+  local seperator                 = {
     orientation = 'vertical',
     forced_width = dpi(5),
     color = beautiful.bg_focus,
     widget = wibox.widget.separator
   }
 
-  local cpu_meter          = {
-    icon({ icon = '🔥', label = 'CPU:' }),
+  local layout_box_widget         = require('widget.layoutbox')(s)
+  local clock_widget              = require('widget.clock')(s)
+  local hard_drives_widget        = require('widget.harddrive-meter').widget
+  local weather_widget            = require('widget.weather.weather-aw').widget
+  local systray_widget            = require('widget.systray').widget
+  local spotify_widget            = require('widget.spotify')
+  local updater_widget            = require('widget.package-updater')()
+  local screen_rec_widget         = require('widget.screen-recorder')()
+  local info_center_toggle_widget = require('widget.info-center-toggle')()
+  local net_speed_widget          = require('widget.network-speed')
+  local ram_widget                = require('widget.ram-widget.ram-widget')({})
+  local cpu_widget                = {
     require('widget.cpu-meter-top-panel.histo').widget,
-    seperator,
     require('widget.cpu-meter-top-panel.kern').widget,
-    seperator,
     spacing = dpi(2),
     layout = wibox.layout.fixed.horizontal
   }
+  local color_palette_widget      = require('widget.color-palette.popup')
 
-  local color_palette      = nil
-  if config.debug_mode then
-    color_palette = require('widget.color-palette.popup')
+  local function setWidgetsFromConfig(widgets, config)
+    dump(config)
+    for i = 1, #config do
+      local widget_name = config[i]
+      log('add widget ' .. tostring(widget_name))
+
+      if widget_name == 'seperator' then
+        widgets[#widgets + 1] = seperator
+      end
+      if widget_name == 'fire_icon' then
+        widgets[#widgets + 1] = icon({ icon = '🔥' })
+      end
+      if widget_name == 'net_icon' then
+        widgets[#widgets + 1] = icon({ icon = '🌐' })
+      end
+      if widget_name == 'media_icon' then
+        widgets[#widgets + 1] = icon({ icon = '🎹' })
+      end
+      if widget_name == 'clock_icon' then
+        widgets[#widgets + 1] = icon({ icon = '🕰️' })
+      end
+      if widget_name == 'color_palette' then
+        widgets[#widgets + 1] = color_palette_widget
+      end
+      if widget_name == 'layout_box' then
+        widgets[#widgets + 1] = layout_box_widget
+      end
+      if widget_name == 'cpu' then
+        widgets[#widgets + 1] = cpu_widget
+      end
+      if widget_name == 'ram' then
+        widgets[#widgets + 1] = ram_widget
+      end
+      if widget_name == 'net_speed' then
+        widgets[#widgets + 1] = net_speed_widget
+      end
+      if widget_name == 'spotify' then
+        widgets[#widgets + 1] = spotify_widget
+      end
+      if widget_name == 'weather' then
+        widgets[#widgets + 1] = weather_widget
+      end
+      if widget_name == 'updater' then
+        widgets[#widgets + 1] = updater_widget
+      end
+      if widget_name == 'hard_drives' then
+        widgets[#widgets + 1] = hard_drives_widget
+      end
+      if widget_name == 'screen_rec' then
+        widgets[#widgets + 1] = screen_rec_widget
+      end
+      if widget_name == 'systray' then
+        widgets[#widgets + 1] = systray_widget
+      end
+      if widget_name == 'clock' then
+        widgets[#widgets + 1] = clock_widget
+      end
+      if widget_name == 'info_center_toggle' then
+        widgets[#widgets + 1] = info_center_toggle_widget
+      end
+    end
   end
+
 
   local leftWidgets = {
     spacing = dpi(2),
     layout = wibox.layout.fixed.horizontal,
   }
+  setWidgetsFromConfig(leftWidgets, config.widget.left_widgets[1])
+  setWidgetsFromConfig(leftWidgets, config.widget.left_widgets[s.index + 1])
 
-  -- add layout box widget
-  if config.widget.layoutbox.enabled then
-    leftWidgets[#leftWidgets + 1] = layout_box
-    if config.widget.seperator.enabled then
-      leftWidgets[#leftWidgets + 1] = seperator
-    end
-  end
-
-  leftWidgets[#leftWidgets + 1] = color_palette
-  if config.widget.seperator.enabled then
-    leftWidgets[#leftWidgets + 1] = seperator
-  end
-
-  leftWidgets[#leftWidgets + 1] = cpu_meter
-  if config.widget.seperator.enabled then
-    leftWidgets[#leftWidgets + 1] = seperator
-  end
-
-  leftWidgets[#leftWidgets + 1] = icon({ icon = '🔥', label = 'RAM:' })
-  leftWidgets[#leftWidgets + 1] = toolbar_ram_widget
-  if config.widget.seperator.enabled then
-    leftWidgets[#leftWidgets + 1] = seperator
-  end
-
-  leftWidgets[#leftWidgets + 1] = icon({ icon = '🌐' })
-  leftWidgets[#leftWidgets + 1] = net_speed_widget
-  if config.widget.seperator.enabled then
-    leftWidgets[#leftWidgets + 1] = seperator
-  end
-
-  if config.widget.spotify.enabled then
-    leftWidgets[#leftWidgets + 1] = icon({ icon = '🎹' })
-    leftWidgets[#leftWidgets + 1] = spotify_widget
-    if config.widget.seperator.enabled then
-      leftWidgets[#leftWidgets + 1] = seperator
-    end
-  end
-
-  local middleWidgets = nil
+  local middleWidgets = {
+    spacing = dpi(2),
+    layout = wibox.layout.fixed.horizontal,
+  }
+  setWidgetsFromConfig(middleWidgets, config.widget.center_widgets[1])
+  setWidgetsFromConfig(middleWidgets, config.widget.center_widgets[s.index + 1])
 
   local rightWidgets = {
     spacing = dpi(5),
     layout = wibox.layout.fixed.horizontal,
   }
+  setWidgetsFromConfig(rightWidgets, config.widget.right_widgets[1])
+  setWidgetsFromConfig(rightWidgets, config.widget.right_widgets[s.index + 1])
 
-  rightWidgets[#rightWidgets + 1] = seperator
-  rightWidgets[#rightWidgets + 1] = weather
-  rightWidgets[#rightWidgets + 1] = seperator
-  rightWidgets[#rightWidgets + 1] = updater
-  rightWidgets[#rightWidgets + 1] = seperator
-  rightWidgets[#rightWidgets + 1] = hard_drives
-  rightWidgets[#rightWidgets + 1] = seperator
-  rightWidgets[#rightWidgets + 1] = screen_rec
-  rightWidgets[#rightWidgets + 1] = seperator
-  rightWidgets[#rightWidgets + 1] = systray
-  rightWidgets[#rightWidgets + 1] = seperator
-  rightWidgets[#rightWidgets + 1] = clock
-  rightWidgets[#rightWidgets + 1] = seperator
-  rightWidgets[#rightWidgets + 1] = info_center_toggle
 
   panel:setup {
     leftWidgets,
